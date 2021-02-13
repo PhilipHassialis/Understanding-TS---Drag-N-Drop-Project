fea55366ca7315765a59252f077208c3e6967050
Philip <philip.alexander.hassialis@gmail.com>
2021-02-13 12:31:20 +0200

    fetching user input

--- src/app.ts
+++ src/app.ts
@@ -29,17 +29,41 @@ class ProjectInput {
         this.element.id = 'user-input'
 
         this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')!;
-        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#people')!;
-        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#description')!;
+        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description')!;
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')!;
 
         this.configure();
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInputs();
+        }
     }
 
     private configure() {
